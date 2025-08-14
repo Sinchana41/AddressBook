a1ac1e317e3047467e351a362513fc38ffa634cd
Sinchana41 <sinchanaan1234@gmail.com>
2025-08-14 13:39:37 +0530

    Use Case Three:Editing contact based on name

--- AddressBook.java
+++ AddressBook.java
@@ -8,4 +8,21 @@ public class AddressBook {
 	void addContact(Contact c) {
 		contacts.add(c); 
 	}
+
+	void editContact(String name, Scanner scanner) {
+		for (Contact contact : contacts) {
+			if (contact.firstName.equalsIgnoreCase(name)) {
+				System.out.println("Editing contact: " + contact);
+				System.out.print("Enter new city: ");
+				contact.city = scanner.nextLine();
+				System.out.print("Enter new phone: ");
+				contact.phone = scanner.nextLine();
+				System.out.print("Enter new email: ");
+				contact.email = scanner.nextLine();
+				System.out.println("Contact updated successfully!");
+				return;
+			}
+		}
+		System.out.println("Contact not found.");
+	}
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,14 +1,28 @@
 package org.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 		System.out.println("*----Welcome to Address Book----*");
+		Scanner scanner=new Scanner(System.in);
 
 		AddressBook book = new AddressBook();
 		book.addContact(new Contact("Sinchana", "A N", "Street 1", "Chikkamanglore", "Karnataka", 12345, "9876543210", "Sinch@mail.com"));
 		System.out.println(book.contacts.get(book.contacts.size()-1));
 		System.out.println("Contact added successfully!");
+		
+        System.out.print("Enter the first name of the contact to edit: ");
+        String name = scanner.nextLine();
+        book.editContact(name, scanner);
+
+        // Display all contacts after edit
+        System.out.println("\nUpdated Contacts:");
+        for (Contact c : book.contacts) {
+            System.out.println(c);
+        }
+
 
 	}
 }
