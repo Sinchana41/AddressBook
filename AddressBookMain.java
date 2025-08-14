da03226fd8a524ce4fee087703a70e2458a3b65c
Sinchana41 <sinchanaan1234@gmail.com>
2025-08-14 14:54:40 +0530

    Use Case Six:Refactor

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,75 +1,117 @@
 package org.addressbook;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class AddressBookMain {
 
+	static Scanner sc = new Scanner(System.in);
+	static Map<String, AddressBook> addressBooks = new HashMap<>();
+
 	public static void main(String[] args) {
-		System.out.println("*----Welcome to Address Book----*");
-		Scanner scanner=new Scanner(System.in);
-
-		AddressBook book = new AddressBook();
-		book.addContact(new Contact("Sinchana", "A N", "Street 1", "Chikkamanglore", "Karnataka", 12345, "9876543210", "Sinch@mail.com"));
-		System.out.println(book.contacts.get(book.contacts.size()-1));
-		System.out.println("Contact added successfully!");
-
-		System.out.print("Enter the first name of the contact to edit: ");
-		String name = scanner.nextLine();
-		book.editContact(name, scanner);
-
-		// Display all contacts after edit
-		System.out.println("\nUpdated Contacts:");
-		for (Contact c : book.contacts) {
-			System.out.println(c);
-		}
-
-		System.out.print("Enter the first name of the contact to delete: ");
-		String firstName = scanner.nextLine();
-		boolean result= book.deleteContact(firstName);
-		if(result) {
-			System.out.println("Contact removed successfully");
-		}else {
-			System.out.println("Contact not found");
-		}
-
-
-		System.out.println("=== Add Multiple Contacts to Address Book ===");
 		while (true) {
-			System.out.print("Enter First Name: ");
-			String fn = scanner.nextLine();
-
-			System.out.print("Enter Last Name: ");
-			String ln = scanner.nextLine();
-
-			System.out.print("Enter Address: ");
-			String addr = scanner.nextLine();
-
-			System.out.print("Enter City: ");
-			String city = scanner.nextLine();
-
-			System.out.print("Enter State: ");
-			String state = scanner.nextLine();
-
-			System.out.print("Enter Zip: ");
-			int zip = Integer.parseInt(scanner.nextLine());
-
-			System.out.print("Enter Phone: ");
-			String phone = scanner.nextLine();
-
-			System.out.print("Enter Email: ");
-			String email = scanner.nextLine();
-
-			// Add contact to address book
-			book.addContact(new Contact(fn, ln, addr, city, state, zip, phone, email));
-
-			System.out.print("Do you want to add another contact? (yes/no): ");
-			String choice = scanner.nextLine();
-			if (choice.equalsIgnoreCase("no")) {
-				break;
+			System.out.println("\n=== Address Book System ===");
+			System.out.println("1. Create New Address Book");
+			System.out.println("2. Add Contact to Address Book");
+			System.out.println("3. Display Contacts of an Address Book");
+			System.out.println("4. Display All Address Books");
+			System.out.println("5. Exit");
+			System.out.print("Enter choice: ");
+			int choice =sc.nextInt();
+			sc.nextLine();
+			switch (choice) {
+			case 1 -> createAddressBook();
+			case 2 -> addContactToBook();
+			case 3 -> displayContactsOfBook();
+			case 4 -> displayAllBooks();
+			case 5 -> deleteContactFromBook();
+			case 6 -> {
+				System.out.println("Exiting...");
+				return;
+			}
+			default -> System.out.println("Invalid choice!");
 			}
 		}
+	}
 
+
+	static void createAddressBook() {
+		System.out.print("Enter unique Address Book name: ");
+		String name= sc.nextLine();
+		if (addressBooks.containsKey(name)) {
+			System.out.println("Address Book already exists!");
+		} else {
+			addressBooks.put(name, new AddressBook());
+			System.out.println("Address Book created.");
+		}
+	}
+
+	static void addContactToBook() {
+		System.out.print("Enter Address Book name: ");
+		String name = sc.nextLine();
+		AddressBook book = addressBooks.get(name);
+		if (book == null) {
+			System.out.println("Address Book not found!");
+			return;
+		}
+		System.out.print("Enter First Name: ");
+		String fn = sc.nextLine();
+
+		System.out.print("Enter Last Name: ");
+		String ln = sc.nextLine();
+
+		System.out.print("Enter Address: ");
+		String addr = sc.nextLine();
+
+		System.out.print("Enter City: ");
+		String city = sc.nextLine();
+
+		System.out.print("Enter State: ");
+		String state = sc.nextLine();
+
+		System.out.print("Enter Zip: ");
+		int zip = Integer.parseInt(sc.nextLine());
+
+		System.out.print("Enter Phone: ");
+		String phone = sc.nextLine();
+
+		System.out.print("Enter Email: ");
+		String email = sc.nextLine();
+
+		book.addContact(new Contact(fn, ln, addr, city, state, zip, phone, email));
+		System.out.println("Contact added to '" + name + "'.");
+	}
+
+	static void displayContactsOfBook() {
+		System.out.print("Enter Address Book name: ");
+		String name = sc.nextLine();
+		AddressBook book = addressBooks.get(name);
+		if (book == null) {
+			System.out.println("Address Book not found!");
+			return;
+		}
 		book.displayContacts();
+	}
 
+	static void deleteContactFromBook() {
+		System.out.print("Enter Address Book name: ");
+		String name = sc.nextLine();
+		AddressBook book = addressBooks.get(name);
+		if (book == null) {
+			System.out.println("Address Book not found!");
+			return;
+		}
+		System.out.println("Enter the First name of contact to delete:");
+		book.deleteContact(sc.nextLine());
+
+	}
+	static void displayAllBooks() {
+		if (addressBooks.isEmpty()) {
+			System.out.println("No Address Books available.");
+			return;
+		}
+		System.out.println("\n--- Address Books ---");
+		for (String name : addressBooks.keySet()) {
+			System.out.println("- " + name);
+		}
 	}
 }
